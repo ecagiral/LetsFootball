35391a4d5bff351e407aeca3c614e42b10fdb9ce
erman cagiral <ecagiral@gmail.com>
2011-12-18 20:45:58 +0200

    Different panels combined in one panel

--- src/com/erman/football/client/gui/MainTab.java
+++ src/com/erman/football/client/gui/MainTab.java
@@ -1,26 +1,66 @@
 package com.erman.football.client.gui;
 
 import com.erman.football.client.cache.Cache;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
 public class MainTab extends HorizontalPanel{
 	
+	private final OtherPanel leftPanel = new OtherPanel();
+	private final OtherPanel listPanel = new OtherPanel();
+	private final OtherPanel detailPanel = new OtherPanel();
+	
+	private final PlayerPanel playerPanel;
+	private final MatchPanel matchPanel;
+	private final PitchPanel pitchPanel;
+	
 	public MainTab(Cache cache){
-		OtherPanel playerDetail = new OtherPanel();
-		OtherPanel matchDetail = new OtherPanel();
-		OtherPanel pitchDetail = new OtherPanel();
-		PlayerPanel playerPanel = new PlayerPanel(cache,playerDetail);
-		MatchPanel matchPanel = new MatchPanel(cache,matchDetail);
-		PitchPanel pitchPanel = new PitchPanel(cache,pitchDetail);
+
+		playerPanel = new PlayerPanel(cache,detailPanel);
+		matchPanel = new MatchPanel(cache,detailPanel);
+		pitchPanel = new PitchPanel(cache,detailPanel);
 	
 		this.setBorderWidth(1);
 		
-		this.add(playerPanel);
-		this.add(playerDetail);
-		this.add(matchPanel);
-		this.add(matchDetail);
-		this.add(pitchPanel);
-		this.add(pitchDetail);
+		Button matchButton = new Button("Maclar");
+		matchButton.addClickHandler(new ClickHandler(){
+
+			public void onClick(ClickEvent event) {
+				listPanel.clear();
+				detailPanel.clear();
+				listPanel.add(matchPanel);
+			}
+			
+		});
+		leftPanel.add(matchButton);
+		Button playerButton = new Button("Oyuncular");
+		playerButton.addClickHandler(new ClickHandler(){
+
+			public void onClick(ClickEvent event) {
+				listPanel.clear();
+				detailPanel.clear();
+				listPanel.add(playerPanel);
+			}
+			
+		});
+		leftPanel.add(playerButton);
+		Button pitchButton = new Button("Sahalar");
+		pitchButton.addClickHandler(new ClickHandler(){
+
+			public void onClick(ClickEvent event) {
+				listPanel.clear();
+				detailPanel.clear();
+				listPanel.add(pitchPanel);
+			}
+			
+		});
+		leftPanel.add(pitchButton);
+		
+		this.add(leftPanel );
+		this.add(listPanel);
+		this.add(detailPanel);
 		
 	}
 
